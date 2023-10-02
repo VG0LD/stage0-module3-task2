d640568c6a4fa9fc37511709e5eca30ac27489aa
VG0LD <117583256+VG0LD@users.noreply.github.com>
2023-10-02 11:24:15 +0600

    Update JavaVariableNames.java

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -2,13 +2,14 @@ package school.mjc.stage0.module3.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int tomorrow = 0;
-        int money = 10;
-        int onehundred = 100;
+        int m2orrow = 0;
+        int money$ = 10;
+        int hundredPercent = 100;
         int back = 1000;
         int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
+
